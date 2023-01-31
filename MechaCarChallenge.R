d63fd0097aa8b77cdfe5fa707e1fee636794bda7
Hanzian <114009272+Hanzian@users.noreply.github.com>
2023-01-30 21:11:45 -0500

    update .R files

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -13,5 +13,20 @@ lm(mpg~vehicle_length + vehicle_weight + spoiler_angle + ground_clearance +AWD, 
 #6. Using the summary() function, determine the p-value and the r-squared value for the linear regression model.
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg))
 
+### CHALLENGE 15: DELIVERABLE 2
 
+#2. Import and read in the Suspension_Coil.csv file as a table
 
+Suspension_Coil <- read.csv(file = '../MechaCar_Statistical_Analysis/Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+#3. Create a total_summary dataframe using the summarize() function
+total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI),
+                                          Median=median(PSI),
+                                          Variance=var(PSI),
+                                          SD=sd(PSI), .groups = 'keep') 
+
+#4. Create a lot_summary dataframe using the group_by() and the summarize() functions to group each manufacturing lot.                                                                
+lot_summary <- Suspension_Coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),
+                                                                         Median=median(PSI),
+                                                                         Variance=var(PSI),
+                                                                         SD=sd(PSI), .groups = 'keep') 
